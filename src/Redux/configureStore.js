448400247cacf80c6201eb20c0aa06bba000b126
Lordkaito <cespedesisai88@gmail.com>
2022-02-16 21:13:44 +0100

    Set up front end side of the app

---
+++ src/Redux/configureStore.js
@@ -0,0 +1,32 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import Axios from 'axios';
+import Logger from 'redux-logger';
+
+const initialState = '';
+
+const GET_GREETING = 'GET_GREETING'
+
+const path = 'http://127.0.0.1:3005/api/v1/greetings';
+
+export const getGreetings = () => async(dispatch) => {
+  const response = await Axios.get(path);
+  let data = { greeting: response.data.greeting };
+  dispatch({ type: GET_GREETING, payload: data });
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_GREETING:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+const configureStore = () =>{
+  const store = createStore(reducer, applyMiddleware(thunk, Logger));
+  return store;
+}
+
+export default configureStore;
